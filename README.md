53dc21fbbb90ff799960ed92fe6ec488b34dcffe
John Watson <john.watson@gmx.co.uk>
2017-07-02 13:28:46 +0100

    Top level parser productions in DSL

--- README.md
+++ README.md
@@ -13,35 +13,35 @@ Front End
 How should we input HSoM melodies to the browser?  It seems to me the best way would be to construct an HSoM DSL with syntax similar to the following:
 
 ```
-    polyphony = music | voices 
-    
-    voices = 'Par' '[' music, { music } ']' 
-    
-    music = prim | line | lines | chord | control music 
-    
-    lines = 'Seq' '[' line, { line } ']' 
-    
-    line = 'Line' '[' chordorprim, { chordorprim } ']' 
-    
-    chordorprim = chord | prim 
-    
-    chord = 'Chord' '[' prim, { prim } ']' 
-    
-    prim = note | rest 
-    
-    note = 'Note' dur pitch vol 
-    
-    rest = 'Rest' dur 
-    
-    dur = 'wn'| 'hn' |'qn'| 'sn' ...... 
-    
-    pitch = pitchClass octave 
-    
-    pitchClass = 'Cff' | 'Cf' | 'C' | 'Cs' | 'Css' | 'Dff' ..... 
-    
-    octave = int 
-    
-    control = 'Instrument' String 
+    polyphony = music | voices
+
+    voices = 'Par' '[' music, { music } ']'
+
+    music = prim | line | lines | chord | control music
+
+    lines = 'Seq' '[' line, { line } ']'
+
+    line = 'Line' '[' chordorprim, { chordorprim } ']'
+
+    chordorprim = chord | prim
+
+    chord = 'Chord' '[' prim, { prim } ']'
+
+    prim = note | rest
+
+    note = 'Note' dur pitch vol
+
+    rest = 'Rest' dur
+
+    dur = 'wn'| 'hn' |'qn'| 'sn' ......
+
+    pitch = pitchClass octave
+
+    pitchClass = 'Cff' | 'Cf' | 'C' | 'Cs' | 'Css' | 'Dff' .....
+
+    octave = int
+
+    control = 'Control' 'Instrument' String 
 ```
 
 This attempts to give a convenient representation for lines of music and chords, whilst still retaining the ability to control whole phrases (however built). If this works out, we can then build in the further control mechanisms that are supported by the API.
@@ -50,4 +50,3 @@ Back End
 --------
 
 HSoM melodies will be converted (via HSoM's __MEvent__) into [Midi](https://github.com/newlandsvalley/purescript-midi). This supports up to 10 channels, each of which can be dedicated to a MIDI instrument.  It should then be possible to play the midi using [purescript-soundfonts](https://github.com/newlandsvalley/purescript-soundfonts) although this will need to be extended (together with the MIDI processing capabilities) in order to cater for more than one instrument.
- 
--- src/Data/Euterpea/DSL/Parser.purs
+++ src/Data/Euterpea/DSL/Parser.purs
@@ -1,18 +1,55 @@
 module Data.Euterpea.DSL.Parser where
 
-import Prelude (($), (<$>), (<$), (<*>), (<*), (*>), (<<<))
+import Prelude (($), (<$>), (<$), (<*>), (<*), (*>), (<<<), (<>))
 import Control.Alt ((<|>))
+import Control.Lazy (fix)
 import Data.String as S
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Int (fromString)
-import Data.List (List(..), singleton)
+import Data.List (List(..), singleton, (:))
+import Data.Array (fromFoldable)
+import Data.Foldable (class Foldable, foldr)
+import Data.Rational (fromInt)
 import Text.Parsing.StringParser (Parser(..), ParseError(..), Pos, try)
-import Text.Parsing.StringParser.String (anyDigit, char, string, regex, skipSpaces)
-import Text.Parsing.StringParser.Combinators (between, choice, many, many1, manyTill, option, optionMaybe, sepBy1, (<?>))
+import Text.Parsing.StringParser.String (anyChar, anyDigit, char, string, regex, skipSpaces)
+import Text.Parsing.StringParser.Combinators (choice, sepBy1, many1, (<?>))
 import Data.Euterpea.Music (Dur, Octave, Pitch(..), PitchClass(..), Primitive(..), Music (..), NoteAttribute(..)) as Eut
 import Data.Euterpea.Music1 as Eut1
 import Data.Euterpea.Notes as Eutn
 import Data.Euterpea.Transform as Eutt
+
+polyphony :: Parser Eut1.Music1
+polyphony =
+  music <|> voices
+
+voices :: Parser Eut1.Music1
+voices =
+  lineToMusic  <$> ((keyWord "Par") *> sepBy1 music (char ','))
+
+{-}
+control :: Parser (Eut1.Music1)
+control =
+  fix $
+    (keyWord "Control") *> (keyWord "Instrument") *> (many1 anyChar)
+-}
+
+
+music :: Parser Eut1.Music1
+music =
+  choice
+    [
+      line
+    , lines
+    , chord
+    ]
+
+lines :: Parser Eut1.Music1
+lines =
+  lineToMusic <$> ((keyWord "Seq") *> sepBy1 line (char ','))
+
+line :: Parser Eut1.Music1
+line =
+  lineToMusic <$> ((keyWord "Line") *> sepBy1 chordOrPrim (char ','))
 
 chordOrPrim :: Parser (Eut1.Music1)
 chordOrPrim = chord <|> prim
@@ -21,10 +58,11 @@ chord :: Parser (Eut1.Music1)
 chord =
   Eutt.chord <$> ((keyWord "Chord") *> sepBy1 primNote1 (char ','))
 
+
 prim :: Parser (Eut1.Music1)
 prim =  Eut.Prim <$> (note1 <|> rest)
 
-primNote1 :: Parser (Eut1.Music1)
+primNote1 :: Parser Eut1.Music1
 primNote1 = Eut.Prim <$> note1
 
 note1 :: Parser (Eut.Primitive Eut1.Note1)
@@ -82,6 +120,7 @@ pitchClass =
     ]
    ) <* skipSpaces
 
+
 css :: Parser Eut.PitchClass
 css = Eut.Css <$ string "Css"
 
@@ -104,6 +143,16 @@ octave =
 volume :: Parser Int
 volume = anyInt <* skipSpaces
 
+anyInt :: Parser Int
+anyInt =
+  (fromMaybe 0 <<< fromString) <$> regex "(0|[1-9][0-9]*)"
+
+anyString :: Parser String
+anyString = fromCharList <$> many1 anyChar
+
+fromCharList :: forall f. Foldable f => f Char -> String
+fromCharList = S.fromCharArray <<< fromFoldable
+
 keyWord :: String -> Parser String
 keyWord target =
   (string target) <* skipSpaces
@@ -111,13 +160,8 @@ keyWord target =
 digit :: Parser Int
 digit = (fromMaybe 0 <<< fromString <<< S.singleton) <$> anyDigit
 
-anyInt :: Parser Int
-anyInt =
-  (fromMaybe 0 <<< fromString) <$> regex "(0|[1-9][0-9]*)"
-
 ten :: Parser Int
 ten = 10 <$ string "10"
-
 
 buildNote1 :: String -> Eut.Dur -> Eut.Pitch -> Int -> Eut.Primitive Eut1.Note1
 buildNote1 _ dur pitch vol =
@@ -125,3 +169,10 @@ buildNote1 _ dur pitch vol =
     note1 = Eut1.Note1 pitch $ singleton (Eut.Volume vol)
   in
     Eut.Note dur note1
+
+-- | Euterpea requires that all lines end in a zero rest as a marker
+-- | which is not how the parser will build naturally lines
+-- | so make sure it's terminated properly
+lineToMusic :: List Eut1.Music1 -> Eut1.Music1
+lineToMusic ms =
+  Eutt.line $ ms <> (singleton <<< Eut.Prim <<< Eut.Rest <<< fromInt) 0
