657608e0e3786fe04bb5b5e152603f9393129218
John Watson <john.watson@gmx.co.uk>
2018-02-14 12:02:41 +0000

    Add save file button
    (Note quite feature complete after all). Add the button to save a PSoM file and reorder the rendered components.  Need also to add a default PSoM button.

--- halogen-editor/src/Container.purs
+++ halogen-editor/src/Container.purs
@@ -3,20 +3,19 @@ module Container where
 import Prelude
 
 import Audio.Euterpea.Playable (PlayablePSoM(..))
-import Audio.SoundFont (AUDIO, Instrument, instrumentChannels, loadRemoteSoundFonts)
-import Audio.SoundFont.Melody.Class (class Playable, toMelody)
+import Audio.SoundFont (AUDIO, Instrument, loadRemoteSoundFonts)
 import Control.Monad.Aff (Aff)
-import Data.Array (cons, length, null)
+import Data.Array (cons, null)
 import Data.Either (Either(..))
-import Data.Either.Nested (Either6)
+import Data.Either.Nested (Either7)
 import Data.Euterpea.DSL.Parser (PSoM, PositionedParseError(..))
 import Data.Foldable (foldl)
-import Data.Functor.Coproduct.Nested (Coproduct6)
+import Data.Functor.Coproduct.Nested (Coproduct7)
 import Data.List (List(..))
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Tuple (fst)
 import Data.MediaType (MediaType(..))
-import Data.Midi.Instrument (InstrumentName(..), gleitzmanName, gleitzmanNames, read)
+import Data.Midi.Instrument (InstrumentName, gleitzmanName, gleitzmanNames, read)
 import Data.Abc.PSoM.DSL (toDSL)
 import Data.Abc.PSoM.Translation (toPSoM)
 import Data.Abc.Parser (parse) as ABC
@@ -27,7 +26,7 @@ import Halogen.Component.ChildPath as CP
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
-import JS.FileIO (FILEIO)
+import JS.FileIO (FILEIO, Filespec, saveTextFile)
 import MultipleSelect.Dom (SDOM)
 import MultipleSelectComponent as MSC
 import Network.HTTP.Affjax (AJAX)
@@ -39,12 +38,14 @@ type AppEffects eff = (ajax :: AJAX, au :: AUDIO, fileio :: FILEIO, sdom :: SDOM
 type State =
   { instruments :: Array Instrument
   , tuneResult :: Either PositionedParseError PSoM
+  , fileName :: Maybe String
   }
 
 data Query a =
     HandlePSoMFile FIC.Message a
   | HandleABCFile FIC.Message a
   | HandleClearButton Button.Message a
+  | HandleSaveButton Button.Message a
   | HandleNewTuneText ED.Message a
   | HandleMultiSelectCommit MSC.Message a
 
@@ -82,7 +83,7 @@ parseError tuneResult =
     Right _ -> "no errors"
     Left (PositionedParseError ppe) -> "parse error: " <> ppe.error
 
-type ChildQuery = Coproduct6 ED.Query FIC.Query FIC.Query MSC.Query Button.Query PC.Query
+type ChildQuery = Coproduct7 ED.Query FIC.Query FIC.Query MSC.Query Button.Query Button.Query PC.Query
 
 -- slots and slot numbers
 type FileInputSlot = Unit
@@ -90,10 +91,11 @@ type MultipleSelectSlot = Unit
 type PlayerSlot = Unit
 type ReplaceInstrumentsSlot = Unit
 type ClearTextSlot = Unit
+type SaveTextSlot = Unit
 type AbcImportSlot = Unit
 type EditorSlot = Unit
 
-type ChildSlot = Either6 Unit Unit Unit Unit Unit Unit
+type ChildSlot = Either7 Unit Unit Unit Unit Unit Unit Unit
 
 editorSlotNo :: CP.ChildPath ED.Query ChildQuery EditorSlot ChildSlot
 editorSlotNo = CP.cp1
@@ -110,8 +112,11 @@ instrumentSelectSlotNo = CP.cp4
 clearTextSlotNo :: CP.ChildPath Button.Query ChildQuery ClearTextSlot ChildSlot
 clearTextSlotNo = CP.cp5
 
+saveTextSlotNo :: CP.ChildPath Button.Query ChildQuery SaveTextSlot ChildSlot
+saveTextSlotNo = CP.cp6
+
 playerSlotNo :: CP.ChildPath PC.Query ChildQuery PlayerSlot ChildSlot
-playerSlotNo = CP.cp6
+playerSlotNo = CP.cp7
 
 
 component ::  ∀ eff. Array Instrument -> H.Component HH.HTML Query Unit Void (Aff (AppEffects eff))
@@ -129,37 +134,55 @@ component instruments =
   initialState instruments =
     { instruments: instruments
     , tuneResult: nullTune
+    , fileName: Nothing
     }
 
   render :: State -> H.ParentHTML Query ChildQuery ChildSlot (Aff (AppEffects eff))
   render state = HH.div_
-    [ HH.div
-        [ HP.class_ (H.ClassName "box")]
-        [ HH.h1_ [ HH.text "Text Area Component" ]
-        , HH.slot' editorSlotNo unit (ED.component "foo") unit (HE.input HandleNewTuneText)
-        ]
-    , HH.div
-        [ HP.class_ (H.ClassName "box")]
-        [ HH.h1_ [ HH.text "Clear Text Component" ]
-        , HH.slot' clearTextSlotNo unit (Button.component "clear") unit (HE.input HandleClearButton)
-        ]
-    , HH.div
-        [ HP.class_ (H.ClassName "box")]
-        [ HH.h1_ [ HH.text "File Input Component 1" ]
-        , HH.slot' psomFileSlotNo unit (FIC.component psomFileInputCtx) unit (HE.input HandlePSoMFile)
-        ]
-    , HH.div
-        [ HP.class_ (H.ClassName "box")]
-        [ HH.h1_ [ HH.text "File Input Component 2" ]
-        , HH.slot' abcImportSlotNo unit (FIC.component abcFileInputCtx) unit (HE.input HandleABCFile)
-        ]
-    , HH.div
-        [ HP.class_ (H.ClassName "box")]
-        [ HH.h1_ [ HH.text "Multiple Select Component" ]
-        , HH.slot' instrumentSelectSlotNo unit (MSC.component initialMultipleSelectState) unit (HE.input HandleMultiSelectCommit)
-        ]
-    , renderPlayer state
-    , renderInstruments state
+    [ HH.div_
+      -- left pane
+      [
+        -- load
+        HH.div
+         [ HP.class_ (H.ClassName "box")]
+         [ HH.h1_ [ HH.text "File Input Component 1" ]
+         , HH.slot' psomFileSlotNo unit (FIC.component psomFileInputCtx) unit (HE.input HandlePSoMFile)
+         ]
+        -- import
+      , HH.div
+         [ HP.class_ (H.ClassName "box")]
+         [ HH.h1_ [ HH.text "File Input Component 2" ]
+         , HH.slot' abcImportSlotNo unit (FIC.component abcFileInputCtx) unit (HE.input HandleABCFile)
+         ]
+        -- save
+      , HH.div
+          [ HP.class_ (H.ClassName "box")]
+          [ HH.h1_ [ HH.text "Save Text Component" ]
+          , HH.slot' saveTextSlotNo unit (Button.component "save") unit (HE.input HandleSaveButton)
+          ]
+        -- clear
+      , HH.div
+          [ HP.class_ (H.ClassName "box")]
+          [ HH.h1_ [ HH.text "Clear Text Component" ]
+          , HH.slot' clearTextSlotNo unit (Button.component "clear") unit (HE.input HandleClearButton)
+          ]
+        -- display instruments
+      , renderInstruments state
+        -- load instruments
+      , HH.div
+          [ HP.class_ (H.ClassName "box")]
+          [ HH.h1_ [ HH.text "Multiple Select Component" ]
+          , HH.slot' instrumentSelectSlotNo unit (MSC.component initialMultipleSelectState) unit (HE.input HandleMultiSelectCommit)
+          ]
+        -- player
+      , renderPlayer state
+      ]
+      -- right pane
+      , HH.div
+          [ HP.class_ (H.ClassName "box")]
+          [ HH.h1_ [ HH.text "Text Area Component" ]
+          , HH.slot' editorSlotNo unit (ED.component "foo") unit (HE.input HandleNewTuneText)
+          ]
     ]
 
   renderPlayer ::  ∀ eff. State -> H.ParentHTML Query ChildQuery ChildSlot (Aff (au :: AUDIO | eff))
@@ -206,11 +229,19 @@ component instruments =
   eval (HandleClearButton (Button.Toggled _) next) = do
     _ <- H.query' editorSlotNo unit $ H.action (ED.UpdateContent "")
     pure next
+  eval (HandleSaveButton (Button.Toggled _) next) = do
+    maybeText <- H.query' editorSlotNo unit (H.request ED.GetText)
+    state <- H.get
+    let
+      fileName = getFileName state
+      text = fromMaybe "" maybeText
+      fsp = { name: fileName, contents : text} :: Filespec
+    _ <- H.liftEff $ saveTextFile fsp
+    pure next
   eval (HandleNewTuneText (ED.TuneResult r) next) = do
     H.modify (\st -> st { tuneResult = r} )
     pure next
   eval (HandleMultiSelectCommit (MSC.CommittedSelections pendingInstrumentNames) next) = do
-    -- state <- H.get
     let
       f acc s =
         case read s of
@@ -221,3 +252,18 @@ component instruments =
     instruments <- H.liftAff $ loadRemoteSoundFonts instrumentNames
     H.modify (\st -> st { instruments = instruments})
     pure next
+
+
+-- helpers
+-- | get the file name
+getFileName :: State -> String
+getFileName state =
+  case state.fileName of
+    Just name ->
+      name
+    _ ->
+      case state.tuneResult of
+        Right { title, music } ->
+          title <> ".psom"
+        _ ->
+          "untitled.psom"
